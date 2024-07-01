8e830383deca8f404e18864d4c3f38e4bbbb1c25
smenasezona <artemkiasko@gmail.com>
2024-07-01 22:09:34 +0300

    add new files for separating logic

---
+++ models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field, field_validator
+from datetime import date
+from typing import Optional
+
+
+class Book(BaseModel):
+    title: str
+    author: str
+    genre: str
+    release_year: int = Field(..., alias="release_date")
+    cover: Optional[str] = None
+
+    @classmethod
+    def validate_year(cls, v):
+        if not (1000 <= v <= 9999):
+            raise ValueError("release_year must be a valid year")
+        return v
+
+    @property
+    def release_date(self) -> date:
+        return date(self.release_year, 1, 1)
